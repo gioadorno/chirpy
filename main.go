381b8b4d7c280a2339fa0f47bbe0fc136c89138d
gioadorno <adorno.gio@gmail.com>
2024-07-05 19:46:44 -0700

    Implement main

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+  const port = "8080"
+
+  mux := http.NewServeMux()
+
+  mux.Handle("/", http.FileServer(http.Dir(".")))
+
+  srv := &http.Server{
+    Addr: ":" + port,
+    Handler: mux,
+  }
+
+
+  log.Printf("Serving on port: %s\n", port)
+  log.Fatal(srv.ListenAndServe())
+}
